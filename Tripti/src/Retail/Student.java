cf35d077975c5147dca0b957d6be59710902a181
Tripti <Tripti@LAPTOP-U0Q2S5PP.Home>
2022-12-14 10:21:45 +0000

    commit and push the code

---
+++ Tripti/src/Retail/Student.java
@@ -0,0 +1,33 @@
+package Retail;
+
+public class Student 
+{
+	int a;
+	
+	public void display()
+	
+	{
+		
+		System.out.println("welcome to all of you in automation");
+	
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+		
+		Student ehika =new Student();
+		ehika.display();
+		ehika.a=10;
+		System.out.println("value of a =" +ehika.a);
+		ehika.a=20;
+		System.out.println("value of a =" +ehika.a);
+		System.out.println("value of a =" +ehika.a  +ehika.a);
+		
+		
+	}
+	
+	
+	
+
+}
